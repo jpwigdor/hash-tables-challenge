1c4533f7764bdc1a1a04182be6632a4206c64b57
James Wigdor <jpwigdor@gmail.com>
2020-07-11 08:03:48 -0400

    exercise 5 complete

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,20 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    paths_by_filename = {}
+
+    for path in files:
+        # this just grabs the last word in the split. grabbing just the `filename` from the `filepath query`
+        filename = path.split('/')[-1]
+        if filename not in paths_by_filename:
+            paths_by_filename[filename] = [path]
+        else:
+            paths_by_filename[filename].append(path)
+
+    result = []
+
+    for query in queries:
+        if query in paths_by_filename:
+            for path in paths_by_filename[query]:
+                result.append(path)
 
     return result
 
